238a5af81e722f8874b76f9bf4a1158360bf305d
Rachel Friedman <rachel@rachelf.com>
2014-02-28 14:44:07 -0500

    Initial commit

---
+++ Keres-android/gen/com/rachelf/keres/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.rachelf.keres;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ Keres-desktop/src/com/rachelf/keres/Main.java
@@ -0,0 +1,16 @@
+package com.rachelf.keres;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class Main {
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "Keres";
+		cfg.useGL20 = false;
+		cfg.width = 800;
+		cfg.height = 480;
+		
+		new LwjglApplication(new Keres(), cfg);
+	}
+}
